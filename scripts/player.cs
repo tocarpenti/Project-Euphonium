5f1f9193779ce0d8b61e9c5dc0f122b5ccbb8670
Tom <tom.carpentier63@gmail.com>
2024-04-10 16:00:11 +0200

    first commit

---
+++ scripts/player.cs
@@ -0,0 +1,52 @@
+using Godot;
+using System;
+
+public partial class player : Area2D
+{
+	[Export]
+	public int speed = 400;
+
+	private Vector2 screen_size;
+
+	private AnimatedSprite2D animator;
+
+	override public void _Ready () {
+		screen_size = GetViewportRect().Size;
+		animator= GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+	}
+
+	override public void _Process (double delta) {
+		Vector2 velocity = Vector2.Zero;
+		if(Input.IsActionPressed("move_right")){
+			velocity.X += 1;
+		}
+		if(Input.IsActionPressed("move_left")){
+			velocity.X -= 1;
+		}
+		if(Input.IsActionPressed("move_up")){
+			velocity.Y -= 1;
+		}
+		if(Input.IsActionPressed("move_down")){
+			velocity.Y += 1;
+		}
+
+		if(velocity.Length() > 0){
+			velocity = velocity.Normalized() * speed;
+			animator.Play();			
+		}
+		else {
+			animator.Stop();
+		}
+
+		Position += velocity * (float) delta;
+		Position = Position.Clamp(Vector2.Zero, screen_size);
+
+		if(velocity.X != 0) {
+			animator.Animation = "walk";
+			animator.FlipH = velocity.X < 0;
+		}
+		else if (velocity.Y != 0){
+			animator.Animation = "up";
+		}
+	}
+}
